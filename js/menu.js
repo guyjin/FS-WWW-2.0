de25e239272e5a1d05a9e0a3ce02bc068f3df4f7
Ben Vaughan <benjamin.vaughan@gmail.com>
2014-07-11 14:39:30 -0600

    fixed menu, added new bg images.

--- js/menu.js
+++ js/menu.js
@@ -2,22 +2,9 @@
  * Created by ben on 7/9/14.
  */
 $(document).ready(function() {
-    $('#navbar a').hover(
+    $('#navbar li').hover(
         function() {
-            var target = $(this).attr("data-menu");
-            console.log($('.dropDownMenu.' + target));
-//            $('.dropDownMenu.' + target).velocity({
-//                top: '48px'
-//            }, 1000, "transition.slideDownOut");
-            $('.dropDownMenu.' + target).velocity(
-               "transition.slideDownIn"
-            );
-        },
-        function() {
-            var target = $(this).attr("data-menu");
-            $('.dropDownMenu.' + target).velocity(
-                "reverse"
-            );
+            $(this).children('.dropDownMenu').slideToggle('fast');
         }
 
     );
