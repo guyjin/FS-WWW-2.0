796d3790ea423d34c4cd38faf381f2998b7e55a1
Ben Vaughan <benjamin.vaughan@gmail.com>
2014-07-22 09:51:44 -0600

    added menu highlights based on class given to body.

--- proto1/index.php
+++ proto1/index.php
@@ -12,7 +12,7 @@
         <link rel="stylesheet" href="css/oldIE.css">
     <![endif]-->
 </head>
-<body lang="en">
+<body lang="en" class="home">
     <div class="flexcontainer">
         <div class="sidebar">
             <header class="header">
@@ -23,11 +23,11 @@
             </header>
             <nav role="navigation">
                 <ul role="menu">
-                    <li role="presentation"><a href="/index.php" role="menuitem" tabindex="1" aria-selected="true">Home</a></li>
-                    <li role="presentation"><a href="/abs/index.php" role="menuitem" tabindex="2" aria-selected="false">Aviation Business System</a></li>
-                    <li role="presentation"><a href="/grants/index.php" role="menuitem" tabindex="3" aria-selected="false">Grants &amp; Agreements</a></li>
-                    <li role="presentation"><a href="/incident/index.php" role="menuitem" tabindex="4" aria-selected="false">Incident Procurement</a></li>
-                    <li role="presentation"><a href="/jobsoverview.php" role="menuitem" tabindex="5" aria-selected="false">Jobs</a></li>
+                    <li role="presentation"><a href="/index.php" role="menuitem" tabindex="1" id="homeLink" class="topLevelLink">Home</a></li>
+                    <li role="presentation"><a href="/abs/index.php" role="menuitem" tabindex="2" id="absLink" class="topLevelLink">Aviation Business System</a></li>
+                    <li role="presentation"><a href="/grants/index.php" role="menuitem" tabindex="3" id="gaLink" class="topLevelLink">Grants &amp; Agreements</a></li>
+                    <li role="presentation"><a href="/incident/index.php" role="menuitem" tabindex="4" id="incLink" class="topLevelLink">Incident Procurement</a></li>
+                    <li role="presentation"><a href="/jobsoverview.php" role="menuitem" tabindex="5" id="jobsLink" class="topLevelLink">Jobs</a></li>
                 </ul>
             </nav>
             <footer role="contentinfo">
