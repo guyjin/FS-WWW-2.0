564c6daf565b1974acf302f7c2ccf8541095156e
Ben Vaughan <benjamin.vaughan@gmail.com>
2014-07-23 14:14:45 -0600

    working on ABS page.  multicolumn layout with flex that allows overflow-y is tough so far.

---
+++ proto1/abs/index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>U.S. Forest Service - Business Operations</title>
+  <base href="/business/2/proto1/">
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/styles.css">
+  <script src="js/jquery-1.11.1.min.js"></script>
+  <script src="js/base.js"></script>
+<!--[if lte IE 9]>
+<link rel="stylesheet" href="css/oldIE.css">
+<![endif]-->
+</head>
+<body lang="en" class="abs">
+  <div class="flexcontainer">
+    <div class="sidebar">
+      <header class="header">
+        <div class="shield">
+          <img src="img/shield.png" alt="image of the U.S. Forest Service Badge/Logo" />
+        </div>
+        <div class="usfs">U.S. Forest Service</div>
+      </header>
+      <nav role="navigation">
+        <ul role="menu">
+          <li role="presentation"><a href="/index.php" role="menuitem" tabindex="1" id="homeLink" class="topLevelLink">Home</a></li>
+          <li role="presentation"><a href="/abs/index.php" role="menuitem" tabindex="2" id="absLink" class="topLevelLink">Aviation Business System</a></li>
+          <li role="presentation"><a href="/grants/index.php" role="menuitem" tabindex="3" id="gaLink" class="topLevelLink">Grants &amp; Agreements</a></li>
+          <li role="presentation"><a href="/incident/index.php" role="menuitem" tabindex="4" id="incLink" class="topLevelLink">Incident Procurement</a></li>
+          <li role="presentation"><a href="/jobsoverview.php" role="menuitem" tabindex="5" id="jobsLink" class="topLevelLink">Jobs</a></li>
+        </ul>
+      </nav>
+      <footer role="contentinfo">
+        <a href="#" class="siteInfoLink" tabIndex="6">
+          <span class="glyphicon glyphicon-cog"></span>
+          Site &amp; Page details
+        </a>
+        <div class="siteDrawer">
+          <ul>
+            <li><a href="http://www.fs.fed.us/disclaimers.shtml">Disclaimers</a></li>
+            <li><a href="http://www.fs.fed.us/privacy.shtml">Privacy Notice</a></li>
+            <li><a href="http://www.fs.fed.us/im/foia/">Freedom of Information Act</a></li>
+          </ul>
+          <hr />
+          <p class="address">
+            USDA Forest Service<br>
+            P.O. Box 96090<br>
+            Washington , D.C. 20090-6090<br>
+            (202) 205-8333
+          </p>
+          <hr />
+          <a href="http://www.firstgov.gov/">FIRST GOV</a>
+          <span class="siteDrawerHandle"><span id="siteDrawerHandleLabel">X</span>
+          <span class="glyphicon glyphicon-remove"></span>
+          <span class="glyphicon glyphicon-arrow-down"></span>
+        </div>
+      </footer>
+    </div>
+    <div class="content">
+      <div class="titlebar" role="banner">
+        Acquistion Management - Aviation Business System
+      </div>
+      <div class="main " role="main">
+        <div class="block-34">
+                    <p>
+                      The <strong>Aviation Business System (ABS)</strong> is a web based application that
+                      will be used by the USDA, Forest Service to electronically document and process all contract
+                      aviation costs currently documented on FS-6500-122 Flight Use Invoice (invoice). 
+                      A disconnected client, non-web limited component version, of the application is
+                      available for remote use.  This new system will require that data will be entered
+                      electronically into ABS from the source location (Helibase, Tanker Base, etc) by
+                      aircraft managers or other designated persons.   From there the following workflow
+                      will be followed:
+                    </p>
+                    <ul class="bulleted">
+                      <li>
+                        The appropriate Contracting Officer (CO) or designated Contracting Officer's
+                        Representative (COR) will review and approve each invoice.
+                      </li>
+                      <li>
+                        The CO or COR will electronically select approved invoices to be packaged for
+                        payment and approve the package using a Personal Identification Number (PIN)
+                        to indicate approval.
+                      </li>
+                      <li>
+                        After approval by the CO or COR, the vendor will receive notification that a
+                        package is ready for approval.
+                      </li>
+                      <li>
+                        After successful review, and application of a PIN by the Vendor, the package
+                        will be submitted to the Albuquerque Service Center (ASC) Incident Database
+                        (IBDB) for final processing of the payment.
+                      </li>
+                      <li>
+                        If the vendor requests any changes to the payment package the changes will be
+                        reviewed by the CO/COR, the changes accepted or rejected and notification of
+                        the results sent to the vendor.  The package is then submitted directly
+                        to the ASC IBDB for payment.
+                      </li>
+                    </ul>
+                              <p>
+                      Access to the system will require an individual USDA eAuthentication account and password
+                      for all users.  In addition, a secure PIN number will be provided to Contracting
+                      Officers (COs) and Contracting Officer Representatives (CORs).  Contractors will be
+                      issued a PIN number by the CO who administers their contracts.  The pin will be
+                      required for electronic approval of payment packages.  In addition each user must
+                      register in ABS the first time they log on.  Please read the step by step
+                      instructions under "Getting Started".
+                    </p>
+
+                    <p>
+                      New users can access training by clicking on the
+                      "<a href="http://www.fs.fed.us/business/abs/training.php">ABS Training</a>"
+                      link on this website.
+                    </p>
+        </div>
+        <div class="block-14">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi doloribus eum officiis voluptatum, provident reiciendis, facilis, voluptatibus necessitatibus dolorum non et suscipit incidunt autem ad? Odit optio quod nemo fugiat.</p>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/jquery-1.11.1.min.js">\x3C/script>')</script>
+</body>
+</html>
